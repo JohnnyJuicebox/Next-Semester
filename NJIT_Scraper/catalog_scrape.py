93e743e649e560f7804f0b5aaba97b9fb349e5ad
Harish Kajur <hkajur93@gmail.com>
2015-03-20 22:31:32 -0400

    Finished course schedule and fixed rate my professor

--- NJIT_Scraper/catalog_scrape.py
+++ NJIT_Scraper/catalog_scrape.py
@@ -1,23 +1,35 @@
-from bs4 import BeautifulSoup
-from bs4 import element
-import json
+from bs4 import BeautifulSoup;
+from bs4 import element;
 import requests
-import sys
+import json
+#import sys
 
 url = 'http://catalog.njit.edu/undergraduate/programs/computerscience.php';
 req = requests.get(url);
 
 soup = BeautifulSoup(req.text);
+
+courselst = []
+mainlst = []
+
 spanlst = soup.find_all('span');
 
 for span in spanlst:
 	if(span.has_attr('class')):
 		if(span['class'][0] == 'degreeTitle'):
+			dct = {};
+			courselst = []
 			if(span.contents):
 				for sp in span.contents:
 					if(type(sp) is element.NavigableString):
-						print sp
+						dct['degreeTitle'] = sp;
+						dct['degreeCourses'] = courselst;
+				mainlst.append(dct);
 		elif(span['class'][0] == 'noDisplay'):
 			if(span.contents):
 				if(type(span.contents[0]) is element.NavigableString):
-					print span.contents[0]
+					courselst.append(span.contents[0]);
+
+
+print json.dumps(mainlst, indent=2)
+
