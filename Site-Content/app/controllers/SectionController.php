a6b61e7f0ad2d0ba619b8496631a28f71ff10c9c
Harish Kajur <hkajur93@gmail.com>
2015-04-08 10:18:29 -0400

    Resolve the issue to get schedule based on time and professor

--- Site-Content/app/controllers/SectionController.php
+++ Site-Content/app/controllers/SectionController.php
@@ -99,7 +99,7 @@ class SectionController extends \BaseController {
         $courses = Input::get('cnames');
         $indexes = array();
         $scheduleId = Schedule::where('user_id', '=', Session::get('user_id'))->get()->all()[0]->id;
-        return $scheduleId;
+        
         DB::delete('DELETE FROM sche_sec_rel WHERE schedule_id = ?', array($scheduleId));
         foreach($courses as $val){
             $indexes[$val] = 0;
